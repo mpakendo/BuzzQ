35b36671896bf53a9d0ecdcc426e339c8bd2aa8b
Martin Pakendorf <martin@loderunner.local>
2014-02-08 18:12:46 +0100

    Minor fix in twitterresults.ejs to create link to twitter user

--- js/BuzzQUI-Responsive.js
+++ js/BuzzQUI-Responsive.js
@@ -86,6 +86,7 @@ function BuzzQUI() {
 
                     html = ejs.render(ui.templates.twitterView, {results: data});
                     $('#BuzzQ-html-Twitter-pane').html(html);
+                    console.log(html);
                     html = ejs.render(ui.templates.instagramView, {results: data});
                     $('#BuzzQ-html-Instagram-pane').html(html);
                     html = ejs.render(ui.templates.flickrView, {results: data});
