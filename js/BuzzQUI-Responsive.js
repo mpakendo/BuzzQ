41ba8c93846b09b4ecf95a36638593a4edfde32d
mpakendo <martin@loderunner.local>
2014-04-06 16:32:01 +0200

    Switch to Twitter Bootstrap 3 and a long scroll UI.

--- js/BuzzQUI-Responsive.js
+++ js/BuzzQUI-Responsive.js
@@ -15,7 +15,11 @@ function BuzzQUI() {
         twitterView: "",
         flickrView: "",
         twitpicView: "",
-        instagramView: ""
+        instagramView: "",
+        layoutLargeEntrySmallList: "",
+        renderTweet: "",
+        renderTwitpic: "",
+        layoutSmallListLargeEntry: ""
     };
     this.debug = false;
 
@@ -39,26 +43,49 @@ function BuzzQUI() {
 
     BuzzQUI.prototype.initUI = function () {
         // load client templates
-        this.templates.twitterView = $.ajax({
-            type:"GET",
-            url:"./clientviews/twitterresults.ejs",
-            async:false
-        }).responseText;
-        this.templates.instagramView = $.ajax({
-            type:"GET",
-            url:"./clientviews/instagramresults.ejs",
-            async:false
-        }).responseText;
-        this.templates.flickrView = $.ajax({
-             type:"GET",
-             url:"./clientviews/flickrresults.ejs",
-             async:false
+        /*
+         this.templates.twitterView = $.ajax({
+         type:"GET",
+         url:"./clientviews/twitterresults.ejs",
+         async:false
          }).responseText;
-        this.templates.twitpicView = $.ajax({
-             type:"GET",
-             url:"./clientviews/twitpicresults.ejs",
-             async:false
+         this.templates.instagramView = $.ajax({
+         type:"GET",
+         url:"./clientviews/instagramresults.ejs",
+         async:false
          }).responseText;
+         this.templates.flickrView = $.ajax({
+         type:"GET",
+         url:"./clientviews/flickrresults.ejs",
+         async:false
+         }).responseText;
+         this.templates.twitpicView = $.ajax({
+         type:"GET",
+         url:"./clientviews/twitpicresults.ejs",
+         async:false
+         }).responseText;
+         */
+        this.templates.renderTweet = $.ajax({
+            type: "GET",
+            url: "./clientviews/renderTweet.ejs",
+            async: false
+        }).responseText;
+        this.templates.layoutLargeEntrySmallList = $.ajax({
+            type: "GET",
+            url: "./clientviews/layoutLargeEntrySmallList.ejs",
+            async: false
+        }).responseText;
+        this.templates.renderTwitpic = $.ajax({
+            type: "GET",
+            url: "./clientviews/renderTwitpic.ejs",
+            async: false
+        }).responseText;
+        this.templates.layoutSmallListLargeEntry = $.ajax({
+            type: "GET",
+            url: "./clientviews/layoutSmallListLargeEntry.ejs",
+            async: false
+        }).responseText;
+
     };
 
     BuzzQUI.prototype.setSearchString = function (event) {
@@ -79,9 +106,68 @@ function BuzzQUI() {
         try {
             var ui = this; // bind 'this' for callback closure
             var url = this.api.url + this.api.queryEndPoint + "?q=" + this.searchString;
-            var callbackFunction =
-                function (data) {
-                    var html = "";
+
+
+            function filter(results,tagStrings){
+                var filteredList = [];
+
+                results.forEach(function(res){
+                    tagStrings.forEach(function(tag) {
+                        if (res.source == tag) {
+                            filteredList.push(res);
+                        }
+                    });
+                });
+
+                return filteredList;
+            }
+
+
+            function callbackFunction(data) {
+                var html = "";
+                var largeLayoutResults = filter(data, ['instagram', 'flickr']);
+                var twitterResults = filter(data, ['twitter']);
+                var twitpicResults = filter(data, ['twitpic']);
+                var count = 0;
+
+                largeLayoutResults.forEach(function (res) {
+                    var model = {largeLayoutEntry: res, smallLayoutEntries: []};
+                    var i = 0;
+                    var a = {};
+                    var renderConfig1 = {
+                        outerTemplate: ui.templates.layoutLargeEntrySmallList,
+                        innerTemplate: ui.templates.renderTweet,
+                        limitSmallEntries: 4,
+                        list: twitterResults
+                    };
+                    var renderConfig2 = {
+                        outerTemplate: ui.templates.layoutSmallListLargeEntry,
+                        innerTemplate: ui.templates.renderTwitpic,
+                        limitSmallEntries: 3,
+                        list: twitpicResults
+                    };
+                    var config = renderConfig1;
+                    var altConfig = renderConfig2;
+
+                    if ((count++ % 2) == 0) {
+                        config = renderConfig2;
+                        altConfig = renderConfig1;
+                    }
+
+                    if (config.list.length== 0) {
+                        config.list = altConfig.list;
+                        config.limitSmallEntries = altConfig.limitSmallEntries;
+                    }
+
+                    while (i < config.limitSmallEntries && ((a = config.list.shift()) != null)) {
+                        model.smallLayoutEntries.push(ejs.render(config.innerTemplate, {model: a}));
+                        i++;
+                    }
+                    html = ejs.render(config.outerTemplate, {model: model});
+                    $('#BuzzQ-html-renderHere').replaceWith(html);
+                });
+
+/*
                     $('#BuzzQ-html-searchAction').popover('destroy');
                     html = ejs.render(ui.templates.twitterView, {results: data});
                     $('#BuzzQ-html-Twitter-pane').html(html);
@@ -91,10 +177,12 @@ function BuzzQUI() {
                     $('#BuzzQ-html-Flickr-pane').html(html);
                     html = ejs.render(ui.templates.twitpicView, {results: data});
                     $('#BuzzQ-html-Twitpic-pane').html(html);
-                };
+*/
+                }
 
             if (!(this.searchString == "")) {
-                $('#BuzzQ-html-searchAction').popover('show');
+                //$('#BuzzQ-html-searchAction').popover('show');
+                $('#BuzzQ-html-results').html('<h3> </h3><div id=\"BuzzQ-html-renderHere\" > <br><h2>Loading..</h2></div>');
                 $.getJSON(url, callbackFunction);
             }
         }
--- js/wireBuzzQ.js
+++ js/wireBuzzQ.js
@@ -5,26 +5,21 @@ function WireBuzzQ() {
 
 (function () {
     WireBuzzQ.prototype.connectHTML =  function (UI) {
-        //debug.println('Wiring UI');
 
-        var fireQuery = function (event) {
+        function fireQuery (event) {
             if (UI.searchString != ""  ) {
-                UI.goFind(event);
+               UI.goFind(event);
             }
-        };
-        $("#BuzzQ-html-Home").attr("href", document.documentURI);
+        }
+
         $("#BuzzQ-html-Brand").attr("href", document.documentURI);
 
-
         $("#BuzzQ-html-searchAction").click(function(event) {
-            //debug.println('Click: BuzzQ-html-searchAction');
             fireQuery(event);
         });
 
        $("#BuzzQ-html-searchString").blur(function(event) {
             UI.setSearchString(event);
-           debug.println('Blur: BuzzQ-html-searchString:'+UI.searchString);
-
         });
 
         /* TODO: Get root cause why keydown causes window.load
@@ -33,7 +28,7 @@ function WireBuzzQ() {
                 UI.setSearchString(event);
                 fireQuery(event);
             }
-        });*/
+        }); */
 
         $("#BuzzQ-html-rssAction").click(function(event) {
             UI.gotoURL(event);
@@ -41,15 +36,19 @@ function WireBuzzQ() {
 
 
         //debug.println('CALLING AJAX URL:'+UI.api.url+UI.api.configEndPoint);
-        $.getJSON(UI.api.url+UI.api.configEndPoint, function(configData) { 
-            if ('true'==configData.debug.debugOn) {
+
+        $.getJSON(UI.api.url+UI.api.configEndPoint, function(configData) {
+            if ('true'==configData.debug.debugOn || configData.debug.debugOn) {
                 $("#BuzzQ-html-debugZone").html("<table><tr>" +
                     "<td><a href='#' onclick='{return debug.displayDebugInfo();}'>Debug Information</a></td>" +
                     "<td><a href='#' onclick='{return debug.clearDebugInfo();}'>Clear Debug Information</a></td>" +
-                    "</tr></table><div style='background:white' id='BuzzQ-html-displayDebugInfo'></div>"
+                    "</tr></table><div style='background:black' id='BuzzQ-html-displayDebugInfo'></div>"
                 );
             }
         });
+
+
+
     }; //WireBuzzQ.prototype.connectHTML
 
 })();
